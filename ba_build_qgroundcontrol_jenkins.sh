c8c0ca22ae85fb57eeb96ba2bb82307c4b186de6
Oleksandr Slovak <slovak194@gmail.com>
2023-09-15 09:22:20 +0200

    + ba build script

---
+++ ba_build_qgroundcontrol_jenkins.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set +eux
+
+# Uncomment this if docker file is not built yet.
+# TODO, put if statement here.
+#docker build --file ./deploy/docker/Dockerfile-build-linux -t qgc-linux-docker .
+
+mkdir -p build
+# sudo chown user:user build
+
+# su - user <<!
+# user
+# whoami
+cd ${WORKSPACE}
+
+docker run --rm -v ${PWD}:/project/source -v ${PWD}/build:/project/build qgc-linux-docker
+
+cd ${WORKSPACE}/build
+../deploy/create_linux_appimage.sh ../ ./staging
